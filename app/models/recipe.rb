ed56ffb9e0e1286b08ac9de01250f3f065341114
phagoth <olexandr.averyanov@gmail.com>
2016-03-23 18:31:55 +0200

    recipes added

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,15 +1,15 @@
 class Recipe
   include HTTParty
 
-  key_value = ENV[’FOOD2FORK_KEY’]
-  hostport  = ENV[’FOOD2FORK_SERVER_AND_PORT’]
+  key_value = ENV['FOOD2FORK_KEY']
+  hostport  = ENV['FOOD2FORK_SERVER_AND_PORT']
 
   base_uri "http://#{hostport}/api"
   default_params key: key_value 
   format :json
 
   def self.for(term, page_number = 1)
-    get("/search", query: {q: term, page: page_number})
+    get("/search", query: {q: term, page: page_number})['recipes']
   end
 
 end
